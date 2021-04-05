e56dfc8376911032b24c785c71320ba3a9beac8f
ghtea <eiirwp@gmail.com>
2021-04-05 20:37:27 +0900

    changed backend url option agains

--- src/apollo.ts
+++ src/apollo.ts
@@ -1,8 +1,16 @@
 import ApolloClient from 'apollo-boost';
 
+const uri = process.env.NODE_ENV === "production" ? 
+  // 프론트엔드 실제 배포했을 때는 당연히 배포된 백엔드 사용
+  process.env.REACT_APP_BACKEND_URL : 
+
+    // 다른 상황에서 내 옵션에 따라 다르게 (기본적으로는 배포된 백엔드 사용)
+    process.env.REACT_APP_BACK_WHICH === 'development' ? 
+    'http://localhost:3001' : 
+      process.env.REACT_APP_BACKEND_URL
+
 const client = new ApolloClient({
-  // 기본적으로는 배포된 백엔드 사용
-  uri: process.env.REACT_APP_BACK_WHICH === 'development' ? 'http://localhost:3001' : process.env.REACT_APP_BACKEND_URL
+  uri: uri
 });
 
 export default client;
